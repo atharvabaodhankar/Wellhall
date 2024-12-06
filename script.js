784b33f25de8aaab6fd3db153ce4804829aa8310
AtharvaPC <baodhankaratharva@gmail.com>
2024-12-06 08:27:13 +0530

    added accommodations

--- script.js
+++ script.js
@@ -21,3 +21,18 @@ gsap.from(".welcome-img img", {
     return -Math.cos(Math.random() * 360 * (Math.PI / 180)) * 30;
   },
 })
+
+
+var accoBoxs = document.querySelectorAll(".accommodations-box");
+var cover = document.querySelector("#cover");
+
+accoBoxs.forEach(box => {
+  box.addEventListener("click", () => {
+    cover.style.display = "flex";
+    cover.querySelector(".cover-img img").src = box.querySelector(".accommodations-img img").src;
+
+  })
+})
+cover.addEventListener("click", () => {
+  cover.style.display = "none";
+})
